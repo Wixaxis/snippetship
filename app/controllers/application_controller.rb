aed63b9882dcef19d5f2942d6bee5201753a6ef4
wixaxis <wixaxis@gmail.com>
2023-01-18 03:10:41 +0100

    devise styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :config_permitted_params, if: :devise_controller?
+
+  protected
+
+  def config_permitted_params
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:username, :first_name, :last_name, :email, :password, :current_password)
+    end
+  end
 end
